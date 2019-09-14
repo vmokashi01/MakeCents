154d248f88638211a6b45486d0b22d04675aa4a3
nicholasc861 <31120213+nicholasc861@users.noreply.github.com>
2019-09-14 04:49:59 -0400

    first version

---
+++ MakeCents.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import config
+
+fromCurrency = 'USD'
+toCurrency = 'CAD'
+amountToConvert = 200032
+
+xe_url = 'https://xecdapi.xe.com/v1/account_info'
+xe_url_change = 'https://xecdapi.xe.com/v1/convert_from.json/'
+
+account_sid = config.ACCOUNT_SID
+auth_token = config.AUTH_KEY
+
+payloadCurrencyConversion = {
+	'from': fromCurrency,
+	'to' : toCurrency,
+	'amount' : amountToConvert
+}
+
+
+response = requests.get(xe_url_change, auth=(account_sid, auth_token), params=payloadCurrencyConversion)
+print(response.text)
